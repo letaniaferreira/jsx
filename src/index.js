d855a176a0eb89d5ce20bfd78c69cad891c914c6
Letania Ferreira <letania@rollbar.com>
2019-06-29 22:36:32 -0700

    Create button

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ import ReactDOM from 'react-dom';
 //Create a React Component
 
 const App = () => {
-	return <div>Hi there chule!</div>;
+	return (
+		<div>
+			<label className="label" htmlFor="name">
+				Enter name;
+			</label>
+			<input id="name" type="text" />
+			<button style={{ backgroundColor:'blue',color: 'white'}}>Submit</button> 
+			</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
