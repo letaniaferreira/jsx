d718678b7de2024b2f526a70cd82fcd8dd459ceb
Letania Ferreira <letania@rollbar.com>
2019-06-29 21:43:15 -0700

    Add public folder and add component to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-//Import react and DOM react
+//Import REact and ReactDOM libraries
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Create a React Component
+
+const App = () => {
+	return <div>Hi there chule!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
